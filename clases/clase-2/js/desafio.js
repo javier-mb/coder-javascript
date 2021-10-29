c7635223da07c53fe1084fb00958deb42041aff1
javier-mb <martinezblanco.jh@gmail.com>
2021-10-28 23:11:58 -0300

    desafio clase-2

---
+++ clases/clase-2/js/desafio.js
@@ -0,0 +1,9 @@
+let edad = parseInt(prompt('Cual es tu edad?'));
+
+if((edad >= 6 && edad <= 11)){
+    console.log('Vas a primaria');
+}else if((edad >= 12 && edad <= 17)){
+    console.log('Vas a secundaria');
+}else{
+    console.log('Vas a la universidad o trabajas');
+}
