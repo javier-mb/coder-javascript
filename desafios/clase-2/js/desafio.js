0f38cd067b1cf103985ab48a039a97c0a8fa651d
javier-mb <martinezblanco.jh@gmail.com>
2021-11-02 09:06:26 -0300

    commit clase-2

--- desafios/clase-2/js/desafio.js
+++ desafios/clase-2/js/desafio.js
@@ -1,8 +1,10 @@
 let edad = parseInt(prompt('Cual es tu edad?'));
 
-if((edad >= 6 && edad <= 11)){
+if(edad >= 0 && edad <= 5){
+    console.log('Vas a guarderia o jardin');
+}else if(edad >= 6 && edad <= 11){
     console.log('Vas a primaria');
-}else if((edad >= 12 && edad <= 17)){
+}else if(edad >= 12 && edad <= 17){
     console.log('Vas a secundaria');
 }else{
     console.log('Vas a la universidad o trabajas');
